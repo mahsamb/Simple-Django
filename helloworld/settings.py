dd5973c1e09632db043a9c0c6f6d87a8dab885e4
Marzieh Babaali <77204200+mahsamb@users.noreply.github.com>
2026-06-06 12:19:25 +0330

    Allow Codespaces host for Django dev server

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pr764cw+c8*(yjx!wgvcs(fsq(00&^_9bz2d5(ke@qqm0kcyd3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.app.github.dev']
 
 
 # Application definition
